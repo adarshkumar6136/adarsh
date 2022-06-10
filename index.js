1d56dd6a42f0232cc9ff98d93b8d634d16837425
adarshkumar6136 <adarshkr282001@gmail.com>
2022-06-11 00:14:51 +0530

    create adarsh

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+const clearConsole=require('clear-any-console');
+clearConsole();
+console.log(`
+adarsh kumar
+currently studying in University of kalyani .Student of Department Of Engineering and
+Technological Studies in stream of Information Technology.now in 6th semester.
+He is interested in learning new things.At present ,he is working on a project 
+regarding node cli.
+
+github: https://github.com/adarshkumar6136/adarsh.git
+
+
+`);
